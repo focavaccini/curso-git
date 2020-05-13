cd6d4209e3b4c5954c061da313c49c908bda0a40
Fernando Cavaccini <fernandocavaccini@gmail.com>
2020-05-13 16:00:27 -0300

    Projeto iniciado, mostrando Ola Mundo

---
+++ Projeto-teste/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Projeto_teste {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá Mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
